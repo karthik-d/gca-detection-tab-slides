c604beb8813772a66fa8c9aea497d600efbb52b5
karthik-d <karthikdesingu2000@gmail.com>
2022-01-24 23:46:17 +0530

    dir structure (pri)

---
+++ src/data/read_svs.py
@@ -0,0 +1,10 @@
+import os
+import openslide
+#from config import config
+
+# Store path-to directory containing .svs files to be converted 
+BASE_PATH = None
+# filenames to be converted
+FILES = [None
+
+]
