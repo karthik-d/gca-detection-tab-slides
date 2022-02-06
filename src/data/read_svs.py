c88c9cfb7bb5375521920dbef7376332ad39c629
karthik-d <karthikdesingu2000@gmail.com>
2022-02-06 23:50:05 +0530

    Patch save done

--- src/data/read_svs.py
+++ src/data/read_svs.py
@@ -9,7 +9,8 @@ from PIL import Image
 BASE_PATH = os.path.abspath(os.path.join(__file__, os.path.pardir, os.path.pardir, os.path.pardir, 'dataset', 'data'))
 # filenames to be converted
 FILES = [
-    "mixed_13829$2000-050-10$US$SCAN$OR$001 -001.tiff",
+    #"mixed_13829$2000-050-10$US$SCAN$OR$001 -001.tiff",
+    "sample.svs"
 ]
 
 # TODO: Move temp directory to data dir
@@ -21,25 +22,27 @@ def make_temp_arrfile(filename, shape, dtype, mode='w+'):
     """
     tempdir = tempfile.TemporaryDirectory()
     # Prepare file
-    buff_shape = tuple([1 for _ in shape])
     file_destn = os.path.join(tempdir.name, 'temp_'+filename)
-    return np.memmap(file_destn, shape=buff_shape, dtype=dtype, mode=mode)
+    return np.memmap(file_destn, shape=shape, dtype=dtype, mode=mode)
 
 
 def get_in_parts(slide, part_size=(4096, 4096), filename='unkown'):
     part_range_x, part_range_y = part_size
-    test_part = np.asarray(slide.read_region((0,0), level=0, size=(1,1)))
+    print(slide.dimensions)
+    test_part = np.asarray(slide.read_region((800, 800), level=0, size=(200,200)))
+    print(test_part.shape)
     # Open accumulator file
     img_acc = make_temp_arrfile(
         filename, 
         shape=(*slide.dimensions, *test_part.shape[2:]), 
         dtype=test_part.dtype
     )
-    img_acc = np.concatenate((img_acc, test_part), axis=-1)
+    img_acc[0:200, 0:200, :] = test_part
+    Image.fromarray(img_acc).save(os.path.join(BASE_PATH, 'check.tiff'))
+    print("HERE")
 
 for filename in FILES:
-    if 'test' not in filename:
-        continue
+    print(os.path.join(BASE_PATH, filename))
     slide = openslide.OpenSlide(os.path.join(BASE_PATH, filename))
     get_in_parts(slide, filename=filename)
     #level_0_img = slide.read_region((0,0), level=0, size=slide.level_dimensions[0])
