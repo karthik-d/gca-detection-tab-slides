d97d3246c365189c85582ad30ca8396ddf8c0736
karthik-d <karthikdesingu2000@gmail.com>
2022-03-01 18:14:16 +0530

    Direct adaptation

---
+++ src/features/extract_roi.py
@@ -0,0 +1,7 @@
+from wsi import slide, tiles, utils
+
+slide.multiprocess_training_slides_to_images()
+# filters.multiprocess_apply_filters_to_images()
+# tiles.multiprocess_filtered_images_to_tiles()
+
+# TODO: Move global constants and config variables to config.py
