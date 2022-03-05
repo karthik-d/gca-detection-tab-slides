8473bb3489c7998d0cfaa9bd2f13648ec2967b4d
karthik-d <karthikdesingu2000@gmail.com>
2022-03-05 14:59:09 +0530

    directo cntr extract

--- src/features/extract_roi.py
+++ src/features/extract_roi.py
@@ -2,9 +2,9 @@ from wsi import slide, utils, filters, roi
 
 # slide.multiprocess_training_slides_to_images()
 # slide.singleprocess_training_slides_to_images()
-filters.singleprocess_apply_filters_to_images()
+# filters.singleprocess_apply_filters_to_images()
 # filters.multiprocess_apply_filters_to_images(display=False)
-# roi.singleprocess_extract_roi_from_filtered()
+roi.singleprocess_extract_roi_from_filtered()
 
 # TODO: Use .tiff+lzw in place of .png
 # TODO: Move global constants and config variables to config.py
--- src/features/wsi/roi.py
+++ src/features/wsi/roi.py
@@ -2,17 +2,31 @@ import multiprocessing
 import numpy as np
 import os 
 import ntpath
+from skimage import measure
 
 import matplotlib.pyplot as plot
 
-from wsi import slide, utils
+from wsi import slide, filters, utils
 from wsi.utils import Time
 
 
 def extract_roi_from_image(slide_filepath):
 	img_path = slide.get_filter_image_result_path(slide_filepath)
 	np_orig = slide.open_image_np(img_path)
-	plot.imshow(np_orig)
+	np_gray = filters.filter_rgb_to_grayscale(np_orig)
+	
+	contours = measure.find_contours(np_gray)
+
+	# Display the image and plot all contours found
+	fig, ax = plot.subplots()
+	ax.imshow(np_gray, cmap=plot.cm.gray)
+
+	for contour in contours:
+		ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+
+	ax.axis('image')
+	ax.set_xticks([])
+	ax.set_yticks([])
 	plot.show()
 	return slide_filepath
 
