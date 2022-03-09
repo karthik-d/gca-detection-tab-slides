73b25b43ad43ef76fc101420230d7e918a634fef
karthik-d <karthikdesingu2000@gmail.com>
2022-03-09 18:53:16 +0530

    downscaled extraction checked

--- src/features/extract_roi.py
+++ src/features/extract_roi.py
@@ -4,7 +4,7 @@ from wsi import slide, utils, filters, roi
 # slide.singleprocess_training_slides_to_images()
 # filters.singleprocess_apply_filters_to_images()
 # filters.multiprocess_apply_filters_to_images(display=False)
-roi.singleprocess_extract_roi_from_filtered(display=True)
+roi.singleprocess_extract_roi_from_filtered()
 # roi.multiprocess_extract_roi_from_filtered()
 
 # TODO: Use .tiff+lzw in place of .png
