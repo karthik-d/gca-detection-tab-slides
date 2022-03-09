6b37064b94f11e8391d09690d686eb32fe3504be
karthik-d <karthikdesingu2000@gmail.com>
2022-03-09 18:40:58 +0530

    morphology fixed

--- src/features/extract_roi.py
+++ src/features/extract_roi.py
@@ -4,10 +4,11 @@ from wsi import slide, utils, filters, roi
 # slide.singleprocess_training_slides_to_images()
 # filters.singleprocess_apply_filters_to_images()
 # filters.multiprocess_apply_filters_to_images(display=False)
-roi.singleprocess_extract_roi_from_filtered()
+roi.singleprocess_extract_roi_from_filtered(display=True)
 # roi.multiprocess_extract_roi_from_filtered()
 
 # TODO: Use .tiff+lzw in place of .png
 # TODO: Move global constants and config variables to config.py
 # TODO: Change directory prefix and suffix
 # TODO: Split boolean arg `save` into save_intermediate and save_final
+# TODO: Add 'level' arg to find_contours() method
