d7416e34a99295d2086e2d661a9e60fd4ee59d45
karthik-d <karthikdesingu2000@gmail.com>
2022-04-22 22:51:35 +0530

    prelim extraction

---
+++ src/metadata/split_samples.py
@@ -0,0 +1,37 @@
+import csv 
+import pandas as pd
+import os 
+
+from config import config
+
+fileset_paths = [
+    os.path.join(config.get("METADATA_PATH"), 'fileset_1.txt'),
+    os.path.join(config.get("METADATA_PATH"), 'fileset_2.txt')
+]
+
+samples_path = os.path.join(config.get("METADATA_PATH"), 'mapping_file-sample-class_trial.csv')
+
+split_samples_paths = [
+    os.path.join(config.get("METADATA_PATH"), 'mapping_file-sample-class_trial-' + str(x) + '.csv')
+    for x in range(len(fileset_paths))
+]
+
+def split_samples():
+    with open(samples_path) as f_src:
+        fileset_dfs = [pd.read_csv(filepath) for filepath in fileset_paths]
+        split_sample_files = [open(filepath, 'w') for filepath in split_samples_paths]
+        counters = [0 for x in range(len(fileset_paths))]
+        instance_ctrs = [0 for x in range(len(fileset_paths))]
+        
+        line = f_src.readline()
+        while line:
+            sample_name = line.split(',')[1]
+            for idx, fileset_df in enumerate(fileset_dfs):
+                if [sample_name] in fileset_df.values:
+                    counters[idx] += 1
+                    split_sample_files[idx].write(line)
+            line = f_src.readline()
+        
+        # Close all files
+        _ = map(lambda x: close(x), split_sample_files)
+        print("Saved into sets:", counters)
