eade0303eae0276d842cf7efb2dba54aec300016
karthik-d <karthikdesingu2000@gmail.com>
2022-10-06 01:03:06 +0530

    describe pipelined

--- src/data/driver.py
+++ src/data/driver.py
@@ -1,9 +1,13 @@
 # Pipeline the data procesing steps here, for each phase
 
 from .preprocessor.augment import augment 
+from .organizer.descibe import describe_datafolder
 
 def organize():
     pass
 
 def preprocess():
     augment()
+
+def describe():
+    describe_datafolder()
---
+++ src/data/organizer/describe.py
@@ -0,0 +1,62 @@
+"""
+Generates basic data sample enumeration in the $PWD as "data_description.csv" file, and as STDOUT for 
+`SRC_DIR` which must be of the form:
+
+    [SRC_DIR]/
+        - Slide-X/
+            - Y/
+                *.tiff
+                .
+                .
+            - N/
+                *.tiff
+                .
+                .
+          .
+          .
+          .
+"""
+
+import csv
+import os
+import pandas as pd
+
+# SET THESE PARAMETERS
+
+#--enter
+SRC_PATH = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), *((os.path.pardir,)*3), 
+    "dataset",
+    "data",
+    "roi",
+    "ds_phase_3_raw",
+    "labeled"
+))
+
+#--enter
+label_names = ['Y', 'N', 'E', 'NAR']
+
+
+def describe_datafolder(display=True):
+
+    stats_file = csv.writer(open("data_description.csv", "w"))
+    stats_file.writerow(["slide_name", "roi_name", "filepath", "label"])
+
+    for slide_name in sorted(os.listdir(SRC_PATH)):
+        slide_path = os.path.join(SRC_PATH, slide_name)
+
+        for label in label_names:
+            label_path = os.path.join(slide_path, label)
+
+            for roi_name in sorted(os.listdir(label_path)):
+                roi_path = os.path.join(label_path, roi_name)
+
+                stats_file.writerow([slide_name, roi_name, roi_path, label])
+
+
+    data_df = pd.read_csv("data_description.csv")
+    data_df.info()
+    print(data_df.groupby(['label']).size())
+
+
+
