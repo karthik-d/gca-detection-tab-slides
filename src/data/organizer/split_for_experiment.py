4f35aaea7067102d8146ff499e2dd68e3f780a0f
karthik-d <karthikdesingu2000@gmail.com>
2022-10-06 00:41:59 +0530

    restructured data

--- src/data/organizer/split_for_experiment.py
+++ src/data/organizer/split_for_experiment.py
@@ -2,15 +2,18 @@
 Splits entire data into `training` and `testing`
 
 ds_phase_N_raw/
-|- training/
-|- testing/
+|- labeled
 |- experiment_splits.json
+    |- training
+    |- testing
 
 ds_phase_N/
 |- splits/       (copy of ds_phase_N_raw/training)
     |- train/
     |- valid/
 |- train_splits.json
+    |- train
+    |- valid
 
 """
 
