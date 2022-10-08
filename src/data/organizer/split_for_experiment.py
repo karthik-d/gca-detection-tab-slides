68fae62af20352b4fe2e45edb351fd70b6f15eb5
karthik-d <karthikdesingu2000@gmail.com>
2022-10-08 09:16:21 +0530

    yearwise cumul added

--- src/data/organizer/split_for_experiment.py
+++ src/data/organizer/split_for_experiment.py
@@ -66,17 +66,23 @@ def split_weighted_chronological(data_df):
     sample_weights = data_df.groupby(['slide_name', 'label']).count().unstack(fill_value=0).iloc[:, 0:len(classes_to_split)]
     sample_weights.columns = sample_weights.columns.droplevel(level=0)    
     sample_weights['var_delta'] = sample_weights.apply(var_delta, axis=1)
-    sample_weights.reset_index()
-    print(sample_weights.columns)
+    sample_weights = sample_weights.reset_index()
     
+    # slide-level
     data_df_samples = data_df.loc[:, ['slide_name', 'year']]
-
     data_df_aug = data_df_samples.merge(sample_weights, on='slide_name').drop_duplicates(keep='first')
     data_df_aug = data_df_aug.sort_values(by=['year', 'var_delta'], ascending=[True, True])
-    # data_df_aug.to_csv("temp.csv")
-    print(data_df_aug)
-    print(data_df_aug[classes_to_split].cumsum())
-    print(data_df_aug.join(data_df_aug[classes_to_split].cumsum(), rsuffix='_cumul').to_csv("temp.csv", index=False))
+    data_df_aug.join(data_df_aug[classes_to_split].cumsum(), rsuffix='_cumul').to_csv("slide-wise-cumulation.csv", index=False)
+    
+    # year-level
+    year_weights = data_df.groupby(['year', 'label']).count().unstack(fill_value=0).iloc[:, 0:len(classes_to_split)]
+    year_weights.columns = year_weights.columns.droplevel(level=0)
+    year_weights = year_weights.reset_index()
+    print(year_weights)
+    year_weights.join(year_weights[classes_to_split].cumsum(), rsuffix='_cumul').to_csv("year-wise-cumulation.csv", index=False)
+    # data_df_aug = data_df_samples.merge(sample_weights, on='slide_name').drop_duplicates(keep='first')
+    # data_df_aug = data_df_aug.sort_values(by=['year', 'var_delta'], ascending=[True, True])
+    # data_df_aug.join(data_df_aug[classes_to_split].cumsum(), rsuffix='_cumul').to_csv("slide-wise-cumulation.csv", index=False)
 
     return sample_weights
 
