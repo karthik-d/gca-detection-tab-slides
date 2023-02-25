5883775dfacb4ea98dcb3a1cc4bdef862c2ceb5c
karthik-d <karthikdesingu2000@gmail.com>
2023-02-25 06:04:11 +0530

    added cleanup in mapping merger

--- src/run.py
+++ src/run.py
@@ -1,16 +1,16 @@
 # from data.extract_svs import *
-# from metadata.merge_mappings import *
+from metadata.merge_mappings import *
 from metadata.describe import *
 # from metadata.split_samples import *
 from data.driver import *
 # from train.driver import *
 from visualize.driver import *
 
-# merge_mappings()
+merge_mappings_fsc()
 # split_samples()
 # preprocess()
 # train()
-describe_merged_mapping()
+# describe_merged_mapping()
 # fold_classnames()
 # describe()
 # split_data()
